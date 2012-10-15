43b993fb474571eac80bab29317e4f8e0ca4d747
Mark Harris <mharris@sitelock.com>
2012-10-15 14:12:29 -0400

    Giving credit where it is due.

--- getifaddr/getifaddr.c
+++ getifaddr/getifaddr.c
@@ -12,6 +12,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+	I am not the original author of this script. This was inspired by
+	the man page at getifaddrs(3).
 */
 
 #include <arpa/inet.h>
